81d492c7f30076ceca7c7b27397f7686e436f6f0
efruchter <fruchter.eric@gmail.com>
2012-11-25 12:12:58 -0500

    some notes

--- src/efruchter/tp/util/RenderUtil.java
+++ src/efruchter/tp/util/RenderUtil.java
@@ -9,55 +9,55 @@ import efruchter.tp.entities.Entity;
 import efruchter.tp.entities.Level;
 
 public class RenderUtil {
-
+	
 	private RenderUtil() {
 	}
-
+	
+	//NOTE: framerate bottleneck is trig functions. Fix.
+	
 	public static void drawCircle(float radius, float subdivisions) {
 		GL11.glPushMatrix();
 		{
 			GL11.glScalef(radius, radius, 1);
-
+			
 			GL11.glBegin(GL11.GL_TRIANGLE_FAN);
 			{
 				GL11.glVertex2f(0, 0);
 				for (int i = 0; i <= subdivisions; i++) {
 					double angle = Math.PI * 2 * i / subdivisions;
-					GL11.glVertex2f((float) Math.cos(angle),
-							(float) Math.sin(angle));
+					GL11.glVertex2f((float) Math.cos(angle), (float) Math.sin(angle));
 				}
 			}
 			GL11.glEnd();
-
+			
 		}
 		GL11.glPopMatrix();
 	}
-
+	
 	public static Behavior getShipRenderer(final Color color) {
 		return new Behavior() {
-
+			
 			@Override
 			public void onStart(Entity self, Level l) {
-
+				
 			}
-
+			
 			@Override
 			public void onUpdate(Entity self, Level l, long delta) {
 				GL11.glPushMatrix();
 				{
-					GL11.glColor3f(color.getRed(), color.getGreen(),
-							color.getBlue());
+					GL11.glColor3f(color.getRed(), color.getGreen(), color.getBlue());
 					GL11.glTranslatef(self.x, self.y, 0);
-					RenderUtil.drawCircle(self.radius, 10);
+					RenderUtil.drawCircle(self.radius, 6);
 				}
 				GL11.glPopMatrix();
 			}
-
+			
 			@Override
 			public void onDeath(Entity self, Level l) {
-
+				
 			}
-
+			
 		};
 	}
 }
