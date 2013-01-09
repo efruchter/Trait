d191ed167c7061a881e21cba7f6a354c7eb19164
efruchter <fruchter.eric@gmail.com>
2013-01-09 17:15:45 -0500

    updated number

--- src/efruchter/tp/TraitProjectClient.java
+++ src/efruchter/tp/TraitProjectClient.java
@@ -46,7 +46,7 @@ public class TraitProjectClient extends Applet {
     /*
      * GAME VARS
      */
-    public static final String VERSION = "00.00.00.02";
+    public static final String VERSION = "00.00.00.03";
     private static Level level;
     private static long lastFrame;
     private static int fps;
