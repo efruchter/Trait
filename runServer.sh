b426ee12be7a11378608482c4f4f081242a19de5
efruchter <fruchter.eric@gmail.com>
2013-01-29 18:15:47 -0500

    dddd

--- runServer.sh
+++ runServer.sh
@@ -1,2 +1,2 @@
-`java -server -classpath dep/*:src efruchter.tp.TraitProjectServer` > log.txt
-SendSMS "Trait server is down or has been shut down"
+java -server -classpath dep/*:src efruchter.tp.TraitProjectServer 2>&1 > log.txt
+SendMSG "Trait server is down or has been shut down"
