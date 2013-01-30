c8f20898d7e003d90b9379470da23674788432c9
efruchter <fruchter.eric@gmail.com>
2013-01-30 14:59:03 -0500

    whoops

--- src/efruchter/tp/defaults/ClientDefaults.java
+++ src/efruchter/tp/defaults/ClientDefaults.java
@@ -2,5 +2,5 @@ package efruchter.tp.defaults;
 
 public class ClientDefaults {
     public static final long LEVEL_LENGTH = 5000;
-    public final static boolean LOCAL_SERVER = true;
+    public final static boolean LOCAL_SERVER = false;
 }
