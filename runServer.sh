2509b0146543d580dbf9c544ca57788c3a2d6f8d
toriscope <fruchter.eric@gmail.com>
2013-02-07 00:52:55 -0500

    Update runServer.sh
    Removed specialized commands

--- runServer.sh
+++ runServer.sh
@@ -1,2 +1,1 @@
-java -server -classpath dep/*:src efruchter.tp.TraitProjectServer 2>&1 > log.txt
-SendMSG "Trait server is down or has been shut down"
+java -server -classpath dep/*:src efruchter.tp.TraitProjectServer
